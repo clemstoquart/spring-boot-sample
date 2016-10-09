f1838c491bb2aefa64228c60b3ab6ce70ad626c7
Clement Stoquart <clemstoquart@gmail.com>
2016-10-09 19:00:09 +0200

    Link to Eureka server

--- src/main/java/com/efrei/Application.java
+++ src/main/java/com/efrei/Application.java
@@ -4,10 +4,12 @@ import com.efrei.repository.MovieRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EntityScan(basePackageClasses = { Application.class, Jsr310JpaConverters.class })
 @EnableJpaRepositories(basePackageClasses = MovieRepository.class)
 public class Application {
