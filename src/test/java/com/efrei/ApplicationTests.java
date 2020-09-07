2dc6762a1c0cd08d05a386920633e0de63f2fc3f
Clement Stoquart <clement.stoquart@ekino.com>
2020-09-07 10:08:46 +0200

    chore: migrate to junit 5

--- src/test/java/com/efrei/ApplicationTests.java
+++ src/test/java/com/efrei/ApplicationTests.java
@@ -1,13 +1,13 @@
 package com.efrei;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class ApplicationTests {
+class ApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 
 }
