ce2fe7ce499e0d1f0aeeb4b0a632ad8a6ce2a7dc
Clement Stoquart <clement.stoquart@ekino.com>
2021-01-05 12:13:26 +0100

    chore: remove useless annotation

--- src/main/java/com/efrei/Application.java
+++ src/main/java/com/efrei/Application.java
@@ -1,12 +1,9 @@
 package com.efrei;
 
-import com.efrei.repository.MovieRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses = MovieRepository.class)
 public class Application {
 
     public static void main(String[] args) {
