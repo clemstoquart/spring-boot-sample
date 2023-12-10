cd95cefb24f617e2d842b44c9eedc88caa561e13
Clement Stoquart <clemstoquart@gmail.com>
2023-12-10 23:14:03 +0100

    chore: upgrade spring-cloud to 2023.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ repositories {
 
 dependencyManagement {
     imports {
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.0-RC1")
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.0")
     }
 }
 
