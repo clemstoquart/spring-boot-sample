5a0983cb6edb60672ede0d66173e3ec2f28f8ee7
Clement Stoquart <clemstoquart@gmail.com>
2024-10-06 23:30:52 +0200

    chore: remove unused import

--- src/main/java/com/efrei/repository/MovieRepository.java
+++ src/main/java/com/efrei/repository/MovieRepository.java
@@ -3,8 +3,6 @@ package com.efrei.repository;
 import com.efrei.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
 }
